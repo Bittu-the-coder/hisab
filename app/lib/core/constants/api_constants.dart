788f0e29c3e103a4f628f83491444465fa71a444
bittu-the-coder <bittuprajapati2271@gmail.com>
2026-06-29 01:07:29 +0530

    v1.1.0: Switch baseUrl to production

--- app/lib/core/constants/api_constants.dart
+++ app/lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  // static const String baseUrl = 'https://hisab-api-sage.vercel.app/api';
+  static const String baseUrl = 'https://hisab-api-sage.vercel.app/api';
   // for local development
-  static const String baseUrl = 'http://192.168.31.95:5000/api';
+  // static const String baseUrl = 'http://192.168.31.95:5000/api';
   static const Duration timeout = Duration(seconds: 10);
 }
